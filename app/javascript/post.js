052a03ebc26ab88cc8f4a41484a1fa29d8571c25
iwasatat0107 <it.was07@gmail.com>
2021-01-14 06:11:56 +0900

    レスポンスを表示

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,4 +1,4 @@
-function post (){
+function post() {
   const submit = document.getElementById("submit_btn");
   submit.addEventListener("click", (e) => {
     const formData = new FormData(document.getElementById("new_article"));
@@ -7,10 +7,18 @@ function post (){
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
-      console.log(XHR.response.article);
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const articleText = document.getElementById("article_text");
+      const HTML = `
+        <div class="article">
+          ${ item.text }
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      articleText.value = "";
     };
     e.preventDefault();
   });
 }
 
-window.addEventListener('load', post);
+window.addEventListener("load", post);
